bf9040951e672e4775c591cbe252feefed05e831
Ahmet Burak Can <118999270+burakshaus@users.noreply.github.com>
2025-12-05 21:49:15 +0300

    im back on 05.10.25

--- BuraksTextEditor/src/main/java/bte/burakstexteditor/SimpleNotepad.java
+++ BuraksTextEditor/src/main/java/bte/burakstexteditor/SimpleNotepad.java
@@ -1,7 +1,6 @@
 package bte.burakstexteditor;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
